5ee5e0af8577f496ff4b2c021f2bb64b07658918
Enzo Glauber <enzojob@gmail.com>
2021-04-13 22:02:28 -0300

    continue

--- schema-query/projeto/resolvers/Query.js
+++ schema-query/projeto/resolvers/Query.js
@@ -1,37 +1,6 @@
 const { usuarios, perfis } = require('../data/db')
 
 module.exports = {
-    ola() {
-        return 'Bom dia!'
-    },
-    horaAtual() {
-        return new Date
-    },
-    usuarioLogado(obj) {
-        console.log(obj)
-        return {
-            id: 1,
-            nome: 'Ana da Web',
-            email: 'anadaweb@email.com',
-            idade: 23,
-            salario_real: 1234.56,
-            vip: true
-        }
-    },
-    produtoEmDestaque() {
-        return {
-            nome: 'Notebook Gamer',
-            preco: 4890.89,
-            desconto: 0.5
-        }
-    },
-    numerosMegaSena() {
-        // return [4, 8, 13, 27, 33, 54]
-        const crescente = (a, b) => a - b
-        return Array(6).fill(0)
-            .map(n => parseInt(Math.random() * 60 + 1))
-            .sort(crescente)
-    },
     usuarios() {
         return usuarios
     },
--- schema-query/projeto/resolvers/Usuario.js
+++ schema-query/projeto/resolvers/Usuario.js
@@ -1,9 +1,6 @@
 const { perfis } = require('../data/db')
 
 module.exports = {
-    salario(usuario) {
-        return usuario.salario_real
-    },
     perfil(usuario) {
         const sels = perfis
             .filter(p => p.id === usuario.perfil_id)
